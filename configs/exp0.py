adfb4f64fbb0686cbec827c9fbe4eed4307472b4
Ryan Kish <43594648+ryandouglaskish@users.noreply.github.com>
2024-09-01 00:19:12 -0500

    update exp0 config

--- configs/exp0.py
+++ configs/exp0.py
@@ -1,50 +1,48 @@
 from box import Box
 
-experiment_config = Box({
-        'core': {
-                'experiment_id': 0,
-                'model1_embed_init': 'glorot_uniform',
-                'model2_embed_init': 'glorot_uniform',
-                'start_model_path': None,
-                'lock_weights': False,
-                'plot_title': 'Baseline Model Perplexity',
+experiment_config = Box(
+    {
+        "core": {
+            "experiment_id": 0,
+            "init_seed": 0,
+            "model1_embed_init": "glorot_uniform",
+            "model2_embed_init": "glorot_uniform",
+            "start_model_path": None,
+            "lock_weights": False,
+            "plot_title": "Baseline Model Perplexity",
         },
-        'data': {
-            'dataset_seed': 0,
+        "model": {
+            "d_model": 128,
+            "n_layers": 6,
+            "heads": 8,
+            "norm": 2.0,
+            "seqlen": 512,
         },
-        'model': {
-            'd_model': 128,
-            'n_layers': 6,
-            'heads': 8,
-            'norm': 2.0,
-            'seqlen': 512,
-
+        "training1": {
+            "batch_size": 3,
+            "SGDR": False,
+            "sched": None,
+            "lr": 0.0001,
+            "threshold": 3,  # TODO: look at this
+            "dropout": 0.1,
         },
-        'training1': {
-            'batch_size': 3,
-            'SGDR': False,
-            'sched': None,
-            'lr': 0.0001,
-            'threshold': 3, # TODO: look at this
-            'dropout': 0.1
+        "training2": {
+            "epochs": 40,
+            "verbose": False,
+            "train_subset": None,  # for testing purposes only
         },
-        'training2': {
-            'epochs': 40,
-            'verbose': False,
-            'train_subset': None, # for testing purposes only
+        "device": {
+            "no_cuda": False,
+            "device": "cpu",
         },
-        'device': {
-            'no_cuda': False,
-            'device': "cuda:0", 
-        },
-    #     'set_programmatically': {
-    #         'train': None,
-    #         'valid': None,
-    #         'test': None,
-    #         'optimizer': None,
-    #     }
-    })
-
+        #     'set_programmatically': {
+        #         'train': None,
+        #         'valid': None,
+        #         'test': None,
+        #         'optimizer': None,
+        #     }
+    }
+)
 
 
 #         'experiment_id': 1,
@@ -77,4 +75,3 @@ experiment_config = Box({
 #         'lock_weights': True,
 #         'starter_model_path': 'experiments/1/models/1/checkpoint_40.pt',
 #     }
-
