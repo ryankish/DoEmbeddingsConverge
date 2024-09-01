b8fc0d36a3819d4e6575ed99e838821409c9ad03
Ryan Kish <43594648+ryandouglaskish@users.noreply.github.com>
2024-09-01 00:18:10 -0500

    fix dataset to not rely on opt, just take seqlen as arg

--- data_utils.py
+++ data_utils.py
@@ -1,45 +1,51 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 
-def read_corpus(filename,tokenizer,first_n=None):
+def read_corpus(filename, tokenizer, first_n=None):
     seq = []
-    with open(filename,'rt') as f:
+    with open(filename, "rt") as f:
         for i, line in enumerate(f):
             if first_n and i > first_n:
-                return(seq)
-            line = line.replace('\n','')
+                return seq
+            line = line.replace("\n", "")
             tokens = tokenizer(line)
-            for t in tokens['input_ids']:
+            for t in tokens["input_ids"]:
                 seq.append(t)
-    return(seq)
+    return seq
+
 
 class WikiDataset(Dataset):
-    def __init__(self, opt, data, overlapping=True):
+    def __init__(self, seqlen, data, overlapping=True):
         super().__init__()
-        self.opt = opt
+        self.seqlen = seqlen
         self.data = data
         self.overlapping = overlapping
-        
-   
+
     def __getitem__(self, idx):
         if self.overlapping:
-            return torch.tensor(self.data[idx:idx+self.opt.seqlen]), \
-                torch.tensor(self.data[idx+1:idx+1+self.opt.seqlen])
+            return torch.tensor(self.data[idx : idx + self.seqlen]), torch.tensor(
+                self.data[idx + 1 : idx + 1 + self.seqlen]
+            )
         else:
-            return torch.tensor(self.data[idx*self.opt.seqlen:(idx+1)*self.opt.seqlen]), \
-                torch.tensor(self.data[idx*self.opt.seqlen+1:(idx+1)*self.opt.seqlen+1])
+            return torch.tensor(
+                self.data[idx * self.seqlen : (idx + 1) * self.seqlen]
+            ), torch.tensor(
+                self.data[idx * self.seqlen + 1 : (idx + 1) * self.seqlen + 1]
+            )
 
     def __len__(self):
         if self.overlapping:
-            return len(self.data) - self.opt.seqlen
+            return len(self.data) - self.seqlen
         else:
-            return len(self.data) // self.opt.seqlen
+            return len(self.data) // self.seqlen
 
 
 def create_masks(input_ids):
     seq_len = input_ids.size(1)
     batch_size = input_ids.size(0)
-    mask = ~torch.triu(torch.ones((seq_len, seq_len), device=input_ids.device), diagonal=1).bool()
+    mask = ~torch.triu(
+        torch.ones((seq_len, seq_len), device=input_ids.device), diagonal=1
+    ).bool()
     mask = mask.unsqueeze(0).expand(batch_size, -1, -1)
     return mask
